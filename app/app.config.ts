1888341ec7edfad6f036337a052f5dfa6ceeebc5
Avior P <avior.peretz@navina.ai>
2026-02-06 16:48:33 +0200

    Update app configuration, enhance profile image URLs, and revise personal description

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,40 +1,45 @@
 export default defineAppConfig({
   global: {
     picture: {
-      dark: 'https://media.licdn.com/dms/image/v2/D4D03AQGa06CAWheZOQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1695893405451?e=1759968000&v=beta&t=1IYU3RJu8oWPBSlQg6X2rnsQsQhD9Qe0rrDule5Opqc',
-      light: 'https://media.licdn.com/dms/image/v2/D4D03AQGa06CAWheZOQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1695893405451?e=1759968000&v=beta&t=1IYU3RJu8oWPBSlQg6X2rnsQsQhD9Qe0rrDule5Opqc',
-      alt: 'My profile picture'
+      dark: "https://media.licdn.com/dms/image/v2/D4D03AQGa06CAWheZOQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1695893405451?e=1772064000&v=beta&t=ry5_n3pr_gXe3SEZM7woEnZ-QscH_O8HWBrrgtpiPEw",
+      light:
+        "https://media.licdn.com/dms/image/v2/D4D03AQGa06CAWheZOQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1695893405451?e=1772064000&v=beta&t=ry5_n3pr_gXe3SEZM7woEnZ-QscH_O8HWBrrgtpiPEw",
+      alt: "My profile picture",
     },
-    meetingLink: 'https://wa.link/7nf5rm',
-    email: 'avior767@gmail.com',
-    available: true
+    meetingLink: "https://wa.link/7nf5rm",
+    email: "avior767@gmail.com",
+    available: true,
   },
   ui: {
     colors: {
-      primary: 'rose',
-      neutral: 'neutral'
+      primary: "rose",
+      neutral: "neutral",
     },
     pageHero: {
       slots: {
-        container: 'py-18 sm:py-24 lg:py-32',
-        title: 'mx-auto max-w-xl text-pretty text-3xl sm:text-4xl lg:text-5xl',
-        description: 'mt-2 text-md mx-auto max-w-2xl text-pretty sm:text-md text-muted'
-      }
-    }
+        container: "py-18 sm:py-24 lg:py-32",
+        title: "mx-auto max-w-xl text-pretty text-3xl sm:text-4xl lg:text-5xl",
+        description:
+          "mt-2 text-md mx-auto max-w-2xl text-pretty sm:text-md text-muted",
+      },
+    },
   },
   footer: {
     credits: `Built with Passion • © 2025`,
     colorMode: true,
-    links: [{
-      'icon': 'i-simple-icons-linkedin',
-      'to': 'https://www.linkedin.com/in/avior/',
-      'target': '_blank',
-      'aria-label': 'Avior on LinkedIn'
-    }, {
-      'icon': 'i-simple-icons-github',
-      'to': 'https://github.com/aviorp',
-      'target': '_blank',
-      'aria-label': 'Avior on GitHub'
-    }]
-  }
-})
+    links: [
+      {
+        icon: "i-simple-icons-linkedin",
+        to: "https://www.linkedin.com/in/avior/",
+        target: "_blank",
+        "aria-label": "Avior on LinkedIn",
+      },
+      {
+        icon: "i-simple-icons-github",
+        to: "https://github.com/aviorp",
+        target: "_blank",
+        "aria-label": "Avior on GitHub",
+      },
+    ],
+  },
+});
